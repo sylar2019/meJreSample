cf2b779b5f3d5cf9ead3d0f7cdd752c871068df1
sylar2019 <81623859@qq.com>
2020-09-23 16:33:45 +0800

    by sylar

--- rpc/thrift/thrift-xxx-client/src/main/java/me/sample/rpc/thrift/client/ThriftClientApplication.java
+++ rpc/thrift/thrift-xxx-client/src/main/java/me/sample/rpc/thrift/client/ThriftClientApplication.java
@@ -4,9 +4,7 @@ import me.java.library.rpc.thrift.client.annotation.EnableThriftClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@EnableFeignClients
 @EnableDiscoveryClient
 @EnableThriftClient
 @SpringBootApplication
